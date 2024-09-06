9296804fff8b93c825ca9a5b687fbffbac46f387
K2k-WebDeveloper <itsupport1@k2kitsupport.com>
2024-09-06 11:44:24 +0530

    created all pages

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     
 
-<footer class="bg-white dark:bg-[#1a5d47]">
+<footer class="bg-[#1a5d47] dark:bg-[#1a5d47]">
     <div class="mx-auto w-full max-w-screen-xl">
       <div class="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
         <div>
